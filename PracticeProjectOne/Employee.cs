3d4f72ffb2db7af6c87541f69cdb30319719cfb9
WhitneyMason <113054936+WhitneyMason@users.noreply.github.com>
2022-09-28 01:39:26 -0400

    Add files via upload

---
+++ PracticeProjectOne/Employee.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace PracticeProjectOne
+{
+    public class Employee
+    {
+        public String? Name { get; set; }
+        public DateTime Birthday { get; set; }
+        public Decimal Salary { get; set; }
+    }
+}
+
---
+++ PracticeProjectOne/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using PracticeProjectOne;
+
+Console.WriteLine("Welcome to your new job" +
+    "!");
+
+Console.WriteLine("Please enter your name");
+var name = Console.ReadLine();
+
+Console.WriteLine("Please enter your birthday");
+var birthdayInput = Console.ReadLine();
+
+DateTime birthday;
+
+while(!DateTime.TryParse (birthdayInput, out birthday))
+{
+   Console.WriteLine("Invalid birthday, please try again");
+   birthdayInput = Console.ReadLine();
+}
+
+Console.WriteLine("Please enter your salary");
+var salary = Console.ReadLine();
+
+var employee = new Employee
+{
+    Name = name,
+    Birthday = birthday,
+    Salary = decimal.Parse(salary)
+};
+
+Console.WriteLine($"Welcome to the company, {employee.Name}!" +
+    $"Your salary is {employee.Salary} " +
+    $"and your birthday is {employee.Birthday.ToShortDateString()}.");
+
+
+
